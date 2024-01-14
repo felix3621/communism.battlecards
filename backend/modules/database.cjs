436c720b203a8a04b9145567b823c1f092925dba
Boogaloo3621 <fjellvangsander@gmail.com>
2024-01-14 07:28:05 +0100

    Revert "online database"
    This reverts commit e346047175f5ab7e074086d987e4a789462a6478.

--- backend/modules/database.cjs
+++ backend/modules/database.cjs
@@ -1,7 +1,6 @@
 const { MongoClient } = require("mongodb");
-require('dotenv').config();
 var url = "mongodb://127.0.0.1:27017";
 
-const mDBClient = new MongoClient(process.env.MONGODB_URI);
+const mDBClient = new MongoClient(url);
 
 module.exports = mDBClient
