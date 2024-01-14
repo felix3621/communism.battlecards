e346047175f5ab7e074086d987e4a789462a6478
Boogaloo3621 <fjellvangsander@gmail.com>
2024-01-14 07:22:01 +0100

    online database

--- backend/modules/database.cjs
+++ backend/modules/database.cjs
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb");
+require('dotenv').config();
 var url = "mongodb://127.0.0.1:27017";
 
-const mDBClient = new MongoClient(url);
+const mDBClient = new MongoClient(process.env.MONGODB_URI);
 
 module.exports = mDBClient
